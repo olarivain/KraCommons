767a5fdd07443e86257cfb80cf456eef6d1aed30
Olivier Larivain <olarivain@edmunds.com>
2011-12-31 12:28:25 -0800

    - added category on nsidexpath for keys

---
+++ KraCommons/indexpath/NSIndexPath+NSIndexPath_Key.h
@@ -0,0 +1,15 @@
+//
+//  NSIndexPath+NSIndexPath_Key.h
+//  KraCommons
+//
+//  Created by Larivain, Olivier on 12/31/11.
+//  Copyright (c) 2011 Edmunds. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSIndexPath (NSIndexPath_Key)
+
+@property (nonatomic, readonly) NSString *key;
+
+@end
---
+++ KraCommons/indexpath/NSIndexPath+NSIndexPath_Key.m
@@ -0,0 +1,19 @@
+//
+//  NSIndexPath+NSIndexPath_Key.m
+//  KraCommons
+//
+//  Created by Larivain, Olivier on 12/31/11.
+//  Copyright (c) 2011 Edmunds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "NSIndexPath+NSIndexPath_Key.h"
+
+@implementation NSIndexPath (NSIndexPath_Key)
+
+- (NSString *) key
+{
+  return [NSString stringWithFormat: @"%i_%i", self.row, self.section];
+}
+
+@end
